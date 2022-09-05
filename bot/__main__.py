41275d0c682b25d6cf32aff95374c79a41b89e2b
Moviehubbd <112174137+Moviehubbd@users.noreply.github.com>
2022-09-05 11:13:27 +0600

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -64,7 +64,7 @@ Type /{BotCommands.HelpCommand} to get a list of available commands
 '''
         sendMarkup(start_string, context.bot, update.message, reply_markup)
     else:
-        sendMarkup('Not an Authorized user, deploy your own helios-mirror-leech bot', context.bot, update.message, reply_markup)
+        sendMarkup('JOIN OUR GROUP AND CHANNELS', context.bot, update.message, reply_markup)
 
 def restart(update, context):
     restart_message = sendMessage("Restarting...", context.bot, update.message)
