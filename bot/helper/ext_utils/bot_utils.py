5e346b1ef20d9a0cacd75ebd40b3f0bfb89a2e42
Moviehubbd <112174137+Moviehubbd@users.noreply.github.com>
2022-09-06 01:03:19 +0600

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -166,15 +166,12 @@ def get_readable_message():
                 uname =download.message.from_user.first_name
                 msg += f"\n<b><a href='{download.message.link}'>Source</a>:</b> {uname} | <b>Id :</b> <code>{download.message.from_user.id}</code>"
             else:
-                msg += ''
-            msg += f"\n<code>/{BotCommands.CancelMirror} {download.gid()}</code>"
-            else:
                 if EMOJI_THEME is True:
-                    msg += f"\n<b>╰❎ Cancel: </b><code>/{BotCommands.CancelMirror} {download.gid()}</code>"
                     msg += f"\n<b>├⛓️ Engine :</b> {download.eng()}"
                     msg += f"\n<b>╰📐 Size: </b>{download.size()}"
                 else:
-                    msg += f"\n<b>├ Cancel: </b><code>/{BotCommands.CancelMirror} {download.gid()}</code>"
+                    msg += ''
+                msg += f"\n<code>/{BotCommands.CancelMirror} {download.gid()}</code>"
                     msg += f"\n<b>├ Engine :</b> {download.eng()}"
                     msg += f"\n<b>╰ Size: </b>{download.size()}"
             msg += f"\n<b>_____________________________</b>"
