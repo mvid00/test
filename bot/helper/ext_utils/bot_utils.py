33f67ee09e777fc68408deb6f87d0ff420e78d36
Moviehubbd <112174137+Moviehubbd@users.noreply.github.com>
2022-09-06 01:06:48 +0600

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -166,14 +166,8 @@ def get_readable_message():
                 uname =download.message.from_user.first_name
                 msg += f"\n<b><a href='{download.message.link}'>Source</a>:</b> {uname} | <b>Id :</b> <code>{download.message.from_user.id}</code>"
             else:
-                if EMOJI_THEME is True:
-                    msg += f"\n<b>├⛓️ Engine :</b> {download.eng()}"
-                    msg += f"\n<b>╰📐 Size: </b>{download.size()}"
-                else:
-                    msg += ''
-                msg += f"\n<code>/{BotCommands.CancelMirror} {download.gid()}</code>"
-                    msg += f"\n<b>├ Engine :</b> {download.eng()}"
-                    msg += f"\n<b>╰ Size: </b>{download.size()}"
+                msg += ''
+            msg += f"\n<code>/{BotCommands.CancelMirror} {download.gid()}</code>"
             msg += f"\n<b>_____________________________</b>"
             msg += "\n\n"
             if STATUS_LIMIT is not None and index == STATUS_LIMIT:
