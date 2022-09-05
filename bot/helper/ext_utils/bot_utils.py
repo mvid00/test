5ae77a86289f8e6f5c65aee816b5e4923af5bd9b
Moviehubbd <112174137+Moviehubbd@users.noreply.github.com>
2022-09-06 02:15:03 +0600

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -209,8 +209,8 @@ def get_readable_message():
         if STATUS_LIMIT is not None and tasks > STATUS_LIMIT:
             msg += f"<b>Page:</b> {PAGE_NO}/{pages} | <b>Tasks:</b> {tasks}\n"
             buttons = ButtonMaker()
-            buttons.sbutton("Previous", "status pre")
-            buttons.sbutton("Next", "status nex")
+            buttons.sbutton("↩️Previous", "status pre")
+            buttons.sbutton("↪️Next", "status nex")
             buttons.sbutton("Statistics", str(FOUR))
             button = buttons.build_menu(2)
             return msg + bmsg, button
